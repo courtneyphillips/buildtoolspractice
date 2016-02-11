072495541a13d0813d56b94bc210e81deb7ea081
Courtney Phillips <courtney@epicodus.com>
2016-02-11 10:48:13 -0800

    author gulp task to concat all interface files into allConcat.js, write to /tmp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 gulp.task('runTests', function(){
   return gulp.src('test/ping-pong-tests.js')
@@ -17,4 +18,10 @@ gulp.task('jsBrowserify', function(){
 
 gulp.task('watchJs', function(){
   gulp.watch(['js/*.js', 'test/*.js'], ['runTests', 'jsBrowserify']);
+});
+
+gulp.task('concatInterface', function(){
+  return gulp.src('./js/*-interface.js')
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
 });
