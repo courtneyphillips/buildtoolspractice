ee654975d8517ffdcf74ba5e76c8329e9bae05b6
Courtney Phillips <courtney@epicodus.com>
2016-02-11 15:20:19 -0800

    add api call to weather-interface.js .

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,5 +5,9 @@ $(document).ready(function(){
     var city = $('#location').val();
     $('#location').val('');
     $('.showWeather').text('The city you have chosen is ' + city + ' .');
+    $.get('http://openweathermap.org/data/2.5/weather?q=' + city + '&APIID=' + apiKey,
+    function(response){
+      console.log(response);
+    })
   });
 });
