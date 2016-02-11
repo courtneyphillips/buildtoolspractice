ea6076970bc76f9042f952bbda551b71882f4ddf
Courtney Phillips <courtney@epicodus.com>
2016-02-11 09:36:58 -0800

    create watchJs gulp task to watch all files in tests and js directories, execute runTests gulp task automatically.

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,7 @@ gulp.task('runTests', function(){
   return gulp.src('test/ping-pong-tests.js')
   .pipe(mocha({reporter: 'nyan'}));
 });
+
+gulp.task('watchJs', function(){
+  gulp.watch(['js/*.js', 'test/*.js'], ['runTests']);
+});
